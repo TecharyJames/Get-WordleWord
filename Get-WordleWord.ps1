eb6ad00b422ae6af5c89f53cf9fecaa235cb5701
James Tarran <jtarran@techary.com>
2022-02-07 11:35:31 +0000

    Create Get-WordleWord.ps1

---
+++ Get-WordleWord.ps1
@@ -0,0 +1,12 @@
+$wordlist = (Invoke-WebRequest https://gist.githubusercontent.com/TecharyJames/44607f1c994779da9cd7fba5549a7a79/raw/d1f569f0bbe59ccb64a10acfb28b5f82ab4a74c0/WordlWordlist).content
+$wordlist = $wordlist.split(",")
+
+$WordNumber = 228
+$StartDate = "02/02/2022"
+$TodaysDate = get-date -format dd/MM/yyyy
+
+$WordCount = $wordnumber + (NEW-TIMESPAN -Start $StartDate -End $todaysDate).days
+
+write-output "The wordle word of the day is $($wordlist[$wordcount])"
+
+pause
