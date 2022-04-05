79d028d93cc0333aff81ebf049db44729accf183
James Tarran <jtarran@techary.com>
2022-04-05 09:47:30 +0100

    Update Get-WordleWord.ps1

--- Get-WordleWord.ps1
+++ Get-WordleWord.ps1
@@ -1,11 +1,11 @@
-$wordlist = (Invoke-WebRequest https://gist.githubusercontent.com/TecharyJames/44607f1c994779da9cd7fba5549a7a79/raw/d1f569f0bbe59ccb64a10acfb28b5f82ab4a74c0/WordlWordlist).content
+$wordlist = (Invoke-WebRequest https://gist.githubusercontent.com/TecharyJames/44607f1c994779da9cd7fba5549a7a79/raw/d1f569f0bbe59ccb64a10acfb28b5f82ab4a74c0/WordlWordlist -UseBasicParsing).content
 $wordlist = $wordlist.split(",")
 
 $WordNumber = 228
 $StartDate = "02/02/2022"
 $TodaysDate = get-date -format dd/MM/yyyy
 
-$WordCount = $wordnumber + (NEW-TIMESPAN -Start $StartDate -End $todaysDate).days + 2
+$WordCount = $wordnumber + (NEW-TIMESPAN -Start $StartDate -End $todaysDate).days + 3
 
 write-output "The wordle word of the day is $($wordlist[$wordcount])"
 
